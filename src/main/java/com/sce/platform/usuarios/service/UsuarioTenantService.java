4d34d0e8b18d4227e721a7293608fb7f11365270
luispal7030 <I202224764@cibertec.edu.pe>
2026-08-21 16:36:51 -0500

    implementacon de Tenant

--- src/main/java/com/sce/platform/usuarios/service/UsuarioTenantService.java
+++ src/main/java/com/sce/platform/usuarios/service/UsuarioTenantService.java
@@ -1,8 +1,27 @@
 package com.sce.platform.usuarios.service;
 
+import com.sce.platform.empresas.entity.Tenant;
+import com.sce.platform.usuarios.entity.Usuario;
+import com.sce.platform.usuarios.entity.UsuarioTenant;
+import com.sce.platform.usuarios.entity.UsuarioTenantRole;
+import com.sce.platform.usuarios.repository.UsuarioTenantRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioTenantService {
+
+    private final UsuarioTenantRepository usuarioTenantRepository;
+    public UsuarioTenantService(UsuarioTenantRepository usuarioTenantRepository) {
+        this.usuarioTenantRepository = usuarioTenantRepository;
+    }
+
+    public UsuarioTenant asociar (Tenant tenant, Usuario usuario, UsuarioTenantRole role) {
+
+        UsuarioTenant usuarioTenant = new UsuarioTenant();
+
+
+
+        return
+    }
 
 }
