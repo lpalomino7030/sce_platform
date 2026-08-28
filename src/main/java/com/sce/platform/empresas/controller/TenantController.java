8e7ba6f03e4a3f6de3c706514ea14060465d84ee
luispal7030 <I202224764@cibertec.edu.pe>
2026-08-28 13:37:16 -0500

    patch commit implementacion de slug

--- src/main/java/com/sce/platform/empresas/controller/TenantController.java
+++ src/main/java/com/sce/platform/empresas/controller/TenantController.java
@@ -1,5 +1,6 @@
 package com.sce.platform.empresas.controller;
 
+import com.sce.platform.empresas.service.TenantService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tentant")
 public class TenantController {
 
-    private final
+    private final TenantService tenantService;
+
+    public TenantController(TenantService tenantService) {
+        this.tenantService = tenantService;
+    }
 
 }
